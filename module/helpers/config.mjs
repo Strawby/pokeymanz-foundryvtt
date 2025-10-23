3907cc0e281fdb492a5062910afc19f7b6684852
Danielle <daniellegluhak@gmail.com>
2025-10-22 21:28:32 -0600

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const POKEYMANZ = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+POKEYMANZ.abilities = {
+  str: 'POKEYMANZ.Ability.Str.long',
+  dex: 'POKEYMANZ.Ability.Dex.long',
+  con: 'POKEYMANZ.Ability.Con.long',
+  int: 'POKEYMANZ.Ability.Int.long',
+  wis: 'POKEYMANZ.Ability.Wis.long',
+  cha: 'POKEYMANZ.Ability.Cha.long',
+};
+
+POKEYMANZ.abilityAbbreviations = {
+  str: 'POKEYMANZ.Ability.Str.abbr',
+  dex: 'POKEYMANZ.Ability.Dex.abbr',
+  con: 'POKEYMANZ.Ability.Con.abbr',
+  int: 'POKEYMANZ.Ability.Int.abbr',
+  wis: 'POKEYMANZ.Ability.Wis.abbr',
+  cha: 'POKEYMANZ.Ability.Cha.abbr',
+};
